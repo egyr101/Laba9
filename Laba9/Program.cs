36e0223388555f4e27f6db24581cb4e8dcd842a4
egyr101 <egladyhev4@gmail.com>
2026-01-13 16:34:30 +0500

    Добавьте файлы проекта.

---
+++ Laba9/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Laba9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
